c2df687f6b7e760fa189c7ed0b1d5b718ce07dc9
vurghunimran <vurqunimranli2001@gmail.com>
2026-08-30 18:24:04 +0400

    Delete page.tsx

--- app/telegram/survey/page.tsx
+++
@@ -1,6 +0,0 @@
-import Script from "next/script";
-import TelegramSurveyClient from "./TelegramSurveyClient";
-
-export default function TelegramSurveyPage() {
-  return <><Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" /><TelegramSurveyClient /></>;
-}
